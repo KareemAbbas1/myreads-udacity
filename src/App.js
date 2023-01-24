f67a1a6218fd49b25a84534784e7623fab49bb32
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2023-01-24 19:42:22 +0200

    switch to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import SearchPage from './Components/SearchPage'
 import Home from './Components/Home'
